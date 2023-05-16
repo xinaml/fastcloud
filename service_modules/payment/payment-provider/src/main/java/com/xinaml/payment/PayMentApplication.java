7f1458cd185b5dbc64d90fed7460fe629ef1d0b1
xinaml <xinaml@qq.com>
2023-05-16 15:04:42 +0800

    去掉api测试依赖

--- service_modules/payment/payment-provider/src/main/java/com/xinaml/payment/PayMentApplication.java
+++ service_modules/payment/payment-provider/src/main/java/com/xinaml/payment/PayMentApplication.java
@@ -4,14 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
 
 @EnableDiscoveryClient
 @SpringBootApplication
 @RefreshScope
-@EnableFeignClients("com.xinaml")
 @ComponentScan(basePackages = {"com.xinaml"})
 public class PayMentApplication {
     public static void main(String[] args) {
--- service_modules/payment/payment-provider/src/main/java/com/xinaml/payment/controller/PaymentController.java
+++ service_modules/payment/payment-provider/src/main/java/com/xinaml/payment/controller/PaymentController.java
@@ -1,15 +1,13 @@
 package com.xinaml.payment.controller;
 
-import com.xinaml.order.api.OrderApi;
-import com.xinaml.payment.service.PaymentService;
 import com.xinaml.payment.bean.Payment;
+import com.xinaml.payment.service.PaymentService;
 import com.xinaml.result.CommonResult;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.RestTemplate;
 
 /**
  * @author liguiqin
@@ -21,11 +19,8 @@ import org.springframework.web.client.RestTemplate;
 public class PaymentController {
     @Autowired
     private PaymentService paymentService;
-    @Autowired
-    private OrderApi orderApi;
-
     @Value("${config.info}")
-    private  String configInfo;
+    private String configInfo;
 
     @GetMapping("/config/info")
     public String getConfigInfo() {
@@ -35,6 +30,7 @@ public class PaymentController {
 
     @Value("${server.port}")
     private int port;
+
     @PostMapping("/payment/create")
     public CommonResult create(@RequestBody Payment dept) {
         int i = paymentService.create(dept);
@@ -49,11 +45,9 @@ public class PaymentController {
     @GetMapping("/payment/get/{id}")
     public CommonResult queryById(@PathVariable("id") Integer id) {
         Payment payment = paymentService.queryById(id);
-        CommonResult orderResult= orderApi.getOrderById(id);
-        log.info("***************查询订单成功*********" + orderResult);
         log.info("***************查询成功*********" + payment);
         if (payment != null) {
-            return CommonResult.success(port+"查询成功", payment);
+            return CommonResult.success(port + "查询成功", payment);
         } else {
             return CommonResult.fail("查询失败", null);
         }
