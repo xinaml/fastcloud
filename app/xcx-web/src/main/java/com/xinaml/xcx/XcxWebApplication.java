10ab33dd1aa3d6f6d8f511879f254f10c132ec2f
xinaml <xinaml@qq.com>
2023-05-19 18:29:21 +0800

    更改项目结构

--- app/xcx-web/src/main/java/com/xinaml/xcx/XcxWebApplication.java
+++ app/xcx-web/src/main/java/com/xinaml/xcx/XcxWebApplication.java
@@ -7,11 +7,11 @@ import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
-@EnableDiscoveryClient
-@SpringBootApplication
-@RefreshScope
-@EnableFeignClients({"com.xinaml.order.api","com.xinaml.payment.api","com.xinaml.test.api"})
-@ComponentScan(basePackages = {"com.xinaml.xcx", "com.xinaml.provider"})
+@EnableDiscoveryClient//开启服务发现
+@SpringBootApplication//springboot启动类
+@RefreshScope//开启配置更新功能
+@EnableFeignClients({"com.xinaml.order.api","com.xinaml.payment.api","com.xinaml.test.api"})//开启feign
+@ComponentScan(basePackages = {"com.xinaml.xcx", "com.xinaml.provider"})//com.xinaml.provider扫描provider的配置类,com.xinaml.xcx扫描xcx自身模块的配置类
 public class XcxWebApplication {
     public static void main(String[] args) {
         SpringApplication.run(XcxWebApplication.class, args);
